0626e566ac6753ecc0cc8deadd2e425c68dc0d8f
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-08-31 11:58:53 +0400

    fix modal

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,8 +1,6 @@
 'use client';
-
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
 
 import {
   Dialog,
@@ -15,9 +13,9 @@ import {
 
 import {
   Form,
-  FormField,
-  FormControl,
   FormItem,
+  FormControl,
+  FormField,
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
@@ -25,14 +23,15 @@ import {
 import { Button } from '@/components/ui/button';
 
 import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
 import { useEffect, useState } from 'react';
 
 const formSchema = z.object({
-  name: z.string().min(1, { message: 'server name is required' }),
+  name: z.string().min(1, { message: 'Server name is required' }),
   imageUrl: z.string().min(1, { message: 'server image is required' }),
 });
 const InitialModal = () => {
-  const [isMounted, setIsMounted] = useState(false);
+  const [isMounted,setIsMounted]=useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     defaultValues: {
       name: '',
@@ -40,35 +39,32 @@ const InitialModal = () => {
     },
     resolver: zodResolver(formSchema),
   });
-
+  const isLoading = form.formState.isSubmitting;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
   };
-
-  const isLoading = form.formState.isSubmitting;
-
-  useEffect(() => {
+  useEffect(()=>{
     setIsMounted(true);
-  }, []);
+  },[])
 
-  if (!isMounted) {
+  if(!isMounted) {
     return null;
   }
   return (
     <Dialog open>
       <DialogContent className="bg-white text-black pt-8 px-6">
-        <DialogHeader>
+        <DialogHeader className="space-y-2">
           <DialogTitle className="text-xl text-center">
             Customize your server
           </DialogTitle>
-          <DialogDescription className="text-zinc-500  text-center">
-            Give your server a profile by setting name and image. you can always
-            update them later.
+          <DialogDescription className="text-zinc-500 text-center">
+            Give your server a personality by setting name and image. you can
+            always update them later
           </DialogDescription>
         </DialogHeader>
         <div className="space-y-8">
-          <div className="flex space-y-2 items-ceter justify-center">
-            TODO: Upload Server Image
+          <div className="flex items-center justify-center">
+            TODO: Upload Image
           </div>
           <Form {...form}>
             <form
@@ -81,23 +77,23 @@ const InitialModal = () => {
                 control={form.control}
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="uppercase text-xs text-zinc-500 font-bold">
+                    <FormLabel className="text-xs font-bold uppercase text-zinc-500">
                       Server Name
                     </FormLabel>
                     <FormControl>
                       <Input
                         disabled={isLoading}
+                        placeholder="Enter Server Name"
                         {...field}
-                        placeholder="Enter Server name"
-                        className="border-0 bg-zinc-300/20 focus-visible:ring-0"
+                        className="border-0 bg-zinc-300/20"
                       />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <DialogFooter className="p-3 bg-gray-100">
-                <Button variant="primary">Create</Button>
+              <DialogFooter className="bg-gray-100 p-3">
+                <Button disabled={isLoading} variant="primary">Create</Button>
               </DialogFooter>
             </form>
           </Form>
