0bc60611aac6799bfd4a5a6f8f30195c9d721276
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-09-05 08:47:36 +0400

    chat input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,42 +3,53 @@ import currentProfile from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirectToSignIn } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import ChatInput from '@/components/chat-input';
 
 interface ChannelIdProps {
-  params:{
-    serverId:string;
-    channelId:string;
-  }
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
 
-const ChannelIdPage = async({params}:ChannelIdProps) => {
-  const profile=await currentProfile();
-  if(!profile) {
+const ChannelIdPage = async ({ params }: ChannelIdProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
     return redirectToSignIn();
   }
-  const channel=await db.channel.findUnique({
-    where:{
-      id:params.channelId
-    }
-  })
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
 
-  const member=await db.member.findFirst({
-    where:{
-      profileId:profile.id,
-      serverId:params.serverId
-    }
-  })
+  const member = await db.member.findFirst({
+    where: {
+      profileId: profile.id,
+      serverId: params.serverId,
+    },
+  });
 
-  if(!channel || !member) {
+  if (!channel || !member) {
     return redirect('/');
   }
 
   return (
-    <div className='bg-white dark:bg-[#313338] h-full flex flex-col'>
-      <ChatHeader serverId={channel.serverId} name={channel.name} type='channel' />
+    <div className="bg-white dark:bg-[#313338] max-h-h-full h-full  flex flex-col">
+      <ChatHeader
+        serverId={channel.serverId}
+        name={channel.name}
+        type="channel"
+      />
+      <div className="flex-1">This will be our future messages</div>
+      <ChatInput
+        name={channel.name}
+        apiUrl="/api/socket/messages"
+        query={{ serverId: channel.serverId, channelId: channel.id }}
+        type="channel"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ChannelIdPage
+export default ChannelIdPage;
---
+++ components/chat-input.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import * as z from 'zod'
+import qs from 'query-string'
+import axios from 'axios'
+import {zodResolver} from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField, FormItem } from './ui/form';
+import { Plus, Smile } from 'lucide-react';
+import { Input } from './ui/input';
+
+interface ChatInputProps {
+    name:string;
+    type:'channel'|'conversation';
+    apiUrl:string;
+    query:Record<string,any>
+}
+
+const formSchema=z.object({
+    content:z.string().min(1)
+})
+
+const ChatInput = ({name,type,apiUrl,query}:ChatInputProps) => {
+    const form=useForm<z.infer<typeof formSchema>>({
+        defaultValues:{
+            content:''
+        },
+        resolver:zodResolver(formSchema)
+    })
+    const onSubmit=async(values:z.infer<typeof formSchema>)=>{
+        try{
+        const url=qs.stringifyUrl({
+            url:apiUrl,
+            query
+        });
+        await axios.post(url,values);
+        }catch(err) {
+            console.log(err);
+        }
+    }
+    const isLoading=form.formState.isSubmitting;
+  return (
+    <div className="pb-6">
+        <Form {...form}>
+        <form noValidate onSubmit={form.handleSubmit(onSubmit)} >
+            <FormField
+                control={form.control}
+                name='content'
+                render={({field})=>(
+                    <FormItem>
+                        <FormControl>
+                            <div className='relative'>
+                            <button disabled={isLoading} className='flex items-center h-[24px] w-[24px] justify-center rounded-full  absolute top-4 left-5 bg-zinc-500'>
+                                <Plus/>
+                            </button>
+                            <Input {...field} placeholder={`Message ${type==='conversation'?name:`#${name}`}`} disabled={isLoading} className='px-14 py-9'/>
+                            <button disabled={isLoading} className='flex items-center justify-center bg-zinc-500 rounded-full absolute right-5 top-4'>
+                                <Smile/>
+                            </button>
+                            </div>
+                        </FormControl>
+                    </FormItem>
+                )}
+            />
+        </form>
+        </Form>
+    </div>
+  )
+}
+
+export default ChatInput
